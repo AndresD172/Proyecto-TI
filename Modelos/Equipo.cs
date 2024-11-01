31cda052d5c44f66bac8a21064f2658d2515563c
Alessandro Coghi <coghicortes@gmail.com>
2024-11-01 16:09:24 -0600

    Modelos Equipo y Multa

---
+++ Modelos/Equipo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Equipo
+    {
+
+        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public string NumeroSerie { get; set; }
+
+        public string Descripcion {  get; set; }
+
+        [Required]
+        public string EstadoEquipo { get; set; }
+
+        public bool Estado {  get; set; }
+
+        [ForeignKey("IdCategoriaEquipo")]
+
+        public int IdCategoriaEquipo { get; set; }
+
+        
+
+    }
+}
