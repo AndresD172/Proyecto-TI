c31a6229bac3f86447acf98437eadbd500ee0139
Alejandro Rodriguez Montero <alerodriguez.montero@gmail.com>
2024-11-01 16:08:51 -0600

    Modelos de departamento y seccion

---
+++ Modelos/Prestatario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Prestatario
+    {
+        [Key]
+        public int IdPrestatario { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string NombrePrestatario { get; set; }
+    }
+}
