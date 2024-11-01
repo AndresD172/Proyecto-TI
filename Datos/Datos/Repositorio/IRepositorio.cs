55bb3fcd09b4f56ddf89e613efb19d0d42627941
Andrés Chacón <andreschaconmora3@gmail.com>
2024-11-01 15:05:45 -0600

    Adición de los elementos básicos.

---
+++ Datos/Datos/Repositorio/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        T Obtener(int id);
+
+        IEnumerable<T> ObtenerTodos();
+    }
+}
