f4dc5f03ae0be7dd6bc51785273efc8a2b6be54d
anghelasalgadof@gmail.com <anghelasalgadof@gmail.com>
2024-11-05 10:48:45 -0600

    Repositorios de CategoriaEquipo y de especialidad Controladores de CategoriaEquipo y de especialidad

--- Datos/Datos/ApplicationDbContext.cs
+++ Datos/Datos/ApplicationDbContext.cs
@@ -15,7 +15,6 @@ namespace Datos
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Especialidad> Especialidades { get; set; }
         public DbSet<Seccion> Secciones { get; set; }
-        public DbSet<Usuario>Usuario { get; set; }
         public DbSet<Especialidad> Especialidad { get; set; }
         public DbSet<CategoriaEquipo> CategoriaEquipo { get; set; }
     }
---
+++ Proyecto-TI/Controllers/EspecialidadController.cs
@@ -0,0 +1,86 @@
+﻿using Datos;
+using Modelos;
+using Datos.Datos.Repositorio.IRepositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_TI.Controllers
+{
+    public class EspecialidadController : Controller
+    {
+        private readonly IEspecialidadRepositorio _especialidadRepositorio;
+
+        public EspecialidadController(IEspecialidadRepositorio especialidadRepositorio)
+        {
+            _especialidadRepositorio = especialidadRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            //IEnumerable<Especialidad> lista = _especialidadRepositorio.ObtenerTodos;
+            //return View(lista);
+            return View();
+        }
+
+        //GET UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            return View();
+        }
+
+        //POST UPSERT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Especialidad especialidad)
+        {
+            if (ModelState.IsValid)
+            {
+                //Nuevo
+                if (especialidad.IdEspecialidad == 0)
+                {
+                    _especialidadRepositorio.Agregar(especialidad);
+                }
+                //Editar
+                else
+                {
+                    _especialidadRepositorio.Actualizar(especialidad);
+                }
+                _especialidadRepositorio.GuardarCambios();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
+        //GET ELIMINAR
+        public IActionResult Eliminar(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _especialidadRepositorio.Obtener(id.GetValueOrDefault());
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST ELIMINAR
+        public IActionResult Eliminar(Especialidad especialidad)
+        {
+
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            _especialidadRepositorio.Remover(especialidad);
+            _especialidadRepositorio.GuardarCambios();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
