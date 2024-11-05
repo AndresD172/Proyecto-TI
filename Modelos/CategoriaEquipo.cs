b585f3960f0575406704d573e9b34aa913490dd9
Sharon Salgado <sharon.salgado@uned.cr>
2024-11-04 20:53:07 -0600

    Modelos Especialidad, Usuario y CategoriaEquipo

---
+++ Modelos/CategoriaEquipo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    internal class CategoriaEquipo
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string descripcionEquipo { get; set; }
+
+        [Required]
+        public bool estado { get; set; }
+    }
+}
---
+++ Modelos/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    internal class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string nombreUsuario { get; set; }
+
+        [Required]
+        public string correo { get; set; }
+
+        [Required]
+        public string contrasena { get; set; }
+
+        [Required]
+        public bool estado { get; set; }
+    }
+}
